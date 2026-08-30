2f10d5f19c373d142f8637d49f17a690a1f11816
dexnoj <wgxcm673@gmail.com>
2026-08-30 22:01:28 +0400

    Create dealOs.c

---
+++ dealOs.c
@@ -0,0 +1,14 @@
+void print_char(char c, int x, int y) {
+    volatile char *video = (volatile char*)0xB8000;
+    int offset = (y * 80 + x) * 2;
+    video[offset] = c;
+    video[offset + 1] = 0x0F;
+}
+
+void kernel_main(void) {
+    char msg[] = "Salam, dealOS!";
+    for (int i = 0; msg[i] != 0; i++) {
+        print_char(msg[i], i, 0);
+    }
+    while (1) {}
+}
